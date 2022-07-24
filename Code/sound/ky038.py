383c098a99f04064e93f8288d34119d8a1fe358c
AlirezaT99 <tajmirriahia@yahoo.com>
2022-07-24 22:57:29 +0430

    Fix the sound sensor code

--- Code/sound/ky038.py
+++ Code/sound/ky038.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-channel = 17
+channel = 24
 GPIO.setmode(GPIO.BCM)  # for GPIO numbering, not PIN numbering (BOARD)
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 
 def sound_callback(channel):
     if GPIO.input(channel):
         print("Sound detected")
     else:
-        print("Sound Finished")
+        print("Sound detected")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # BOTH = RISING + FALLING
 GPIO.add_event_callback(channel, sound_callback)
+
+while True:
+    time.sleep(1)
